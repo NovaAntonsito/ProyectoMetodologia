d3be25550137592cf48d388d9216045294d0a415
francoGonzalez2706 <franco.gonzalez.reale.276@gmail.com>
2022-05-20 12:24:57 -0300

    validacion de movimiento peones

--- Chess/Engine.py
+++ Chess/Engine.py
@@ -1,3 +1,6 @@
+import random
+
+
 class EstadoJuego:
     def __init__(self):
         # el talblero es un array de 8x8, donde cada elemento tiene dos caracteres.
@@ -36,6 +39,59 @@ class EstadoJuego:
             self.tablero[movimiento.filaFinal][movimiento.columnaFinal] = movimiento.piezaCapturada
             self.movimientoBlanca = not self.movimientoBlanca
 
+    def traerMovimietosValidos(self):
+        return self.traerTodosMovimientosPosibles()
+
+    '''
+    MOVIMIENTOS
+    '''
+
+    def traerTodosMovimientosPosibles(self):
+        movimientos = []
+        for f in range(len(self.tablero)):
+            for c in range(len(self.tablero[f])):
+                turno = self.tablero[f][c][0]
+                if (turno == 'b' and self.movimientoBlanca) or (
+                        turno == "n" and not self.movimientoBlanca):
+                    print(self.movimientoBlanca)
+                    pieza = self.tablero[f][c][1]
+                    if pieza == 'P':
+                        self.getMovimientoPeon(f, c, movimientos)
+                    elif pieza == 'T':
+                        self.getMovimientoTorre(f, c, movimientos)
+        return movimientos
+
+    '''
+    obtener todos los movimentos por el peon selecionado
+    '''
+
+    def getMovimientoPeon(self, f, c, movimientos):
+        print("getMovimientoPeon", self.movimientoBlanca)
+        if self.movimientoBlanca:
+            if self.tablero[f - 1][c] == "--":
+                movimientos.append(Mover((f, c), (f - 1, c), self.tablero))
+                if f == 6 and self.tablero[f - 2][c] == "--":
+                    movimientos.append(Mover((f, c), (f - 2, c), self.tablero))
+            if c - 1 >= 0:
+                if self.tablero[f - 1][c - 1][0] == 'n':
+                    movimientos.append(Mover((f, c), (f - 1, c - 1), self.tablero))
+            if c + 1 <= 7:
+                if self.tablero[f - 1][c + 1][0] == 'n':
+                    movimientos.append(Mover((f, c), (f - 1, c + 1), self.tablero))
+        else:
+            print("Entre aca=>", f, c)
+            if self.tablero[f + 1][c] == "--":
+                movimientos.append(Mover((f, c), (f + 1, c), self.tablero))
+                if f == 1 and self.tablero[f + 2][c] == "--":
+                    movimientos.append(Mover((f, c), (f + 2, c), self.tablero))
+
+    ''' 
+    obtener todos los movimentos por la torre selecionada
+    '''
+
+    def getMovimientoTorre(self, f, c, moves):
+        pass
+
 
 # Lista de movimientos separados
 class Mover:
@@ -54,6 +110,12 @@ class Mover:
         self.columnaFinal = casFinal[1]
         self.piezaMovida = tablero[self.filaInicial][self.columnaInicial]
         self.piezaCapturada = tablero[self.filaFinal][self.columnaFinal]
+        self.movimientoID = self.filaInicial * 1000 + self.filaFinal * 100 + self.filaFinal * 10 + self.columnaFinal
+
+    def __eq__(self, otro):
+        if isinstance(otro, Mover):
+            return self.movimientoID == otro.movimientoID
+        return False
 
     def getNotacionAjedrez(self):
         return self.getRangoFila(self.filaInicial, self.columnaInicial) + self.getRangoFila(self.filaFinal,
--- Chess/main.py
+++ Chess/main.py
@@ -1,6 +1,5 @@
 import pygame as p
-import Engine  
-
+from Chess import Engine
 
 # Settings iniciales para PYGAME
 p.init()
@@ -13,18 +12,30 @@ IMAGENES = {}
 
 def cargarImagenes():
     # Transformar las imagenes a una escala para el tablero
-    IMAGENES["nA"] = p.transform.scale(p.image.load("imagenes/nA.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["nC"] = p.transform.scale(p.image.load("imagenes/nC.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["nT"] = p.transform.scale(p.image.load("imagenes/nT.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["nQ"] = p.transform.scale(p.image.load("imagenes/nQ.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["nR"] = p.transform.scale(p.image.load("imagenes/nR.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["nP"] = p.transform.scale(p.image.load("imagenes/nP.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["bA"] = p.transform.scale(p.image.load("imagenes/bA.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["bC"] = p.transform.scale(p.image.load("imagenes/bC.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["bT"] = p.transform.scale(p.image.load("imagenes/bT.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["bQ"] = p.transform.scale(p.image.load("imagenes/bQ.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["bR"] = p.transform.scale(p.image.load("imagenes/bR.png"), (SQ_SIZE, SQ_SIZE))
-    IMAGENES["bP"] = p.transform.scale(p.image.load("imagenes/bP.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["nA"] = p.transform.scale(
+        p.image.load("imagenes/nA.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["nC"] = p.transform.scale(
+        p.image.load("imagenes/nC.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["nT"] = p.transform.scale(
+        p.image.load("imagenes/nT.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["nQ"] = p.transform.scale(
+        p.image.load("imagenes/nQ.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["nR"] = p.transform.scale(
+        p.image.load("imagenes/nR.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["nP"] = p.transform.scale(
+        p.image.load("imagenes/nP.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["bA"] = p.transform.scale(
+        p.image.load("imagenes/bA.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["bC"] = p.transform.scale(
+        p.image.load("imagenes/bC.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["bT"] = p.transform.scale(
+        p.image.load("imagenes/bT.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["bQ"] = p.transform.scale(
+        p.image.load("imagenes/bQ.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["bR"] = p.transform.scale(
+        p.image.load("imagenes/bR.png"), (SQ_SIZE, SQ_SIZE))
+    IMAGENES["bP"] = p.transform.scale(
+        p.image.load("imagenes/bP.png"), (SQ_SIZE, SQ_SIZE))
 
 
 def main():
@@ -33,11 +44,12 @@ def main():
     reloj = p.time.Clock()
     pantalla.fill(p.Color(255, 255, 255))
     estadoJuego = Engine.EstadoJuego()
-    cargarImagenes()
+    movimientosValidos = estadoJuego.traerMovimietosValidos()
+    movRealizado = False
     p.display.set_caption("Chess LinuxIsMyPassion")
     logo = p.image.load("imagenes/logo.png")
     p.display.set_icon(logo)
-
+    cargarImagenes()
     ejecutando = True
     posicionAnterior = ()
     clicksJugador = []
@@ -56,16 +68,22 @@ def main():
                     posicionAnterior = (fil, col)
                     clicksJugador.append(posicionAnterior)
                 if len(clicksJugador) == 2:
-                    mover = Engine.Mover(clicksJugador[0], clicksJugador[1], estadoJuego.tablero)
+                    mover = Engine.Mover(
+                        clicksJugador[0], clicksJugador[1], estadoJuego.tablero)
                     print(mover.getNotacionAjedrez())
-                    estadoJuego.hacerMovimiento(mover)
+                    if mover in movimientosValidos:
+                        estadoJuego.hacerMovimiento(mover)
+                        movRealizado = True
                     posicionAnterior = ()
                     clicksJugador = []
             elif e.type == p.KEYDOWN:
-                if e.key == p.K_z: #z esta presionada
+                if e.key == p.K_z:  # z esta presionada
                     estadoJuego.movAnterior()
+                    movRealizado = True
 
-
+        if movRealizado:
+            movimientosValidos = estadoJuego.traerMovimietosValidos()
+            movRealizado = False
 
         dibujarEstado(pantalla, estadoJuego)
         reloj.tick(MAX_FPS)
@@ -75,6 +93,8 @@ def main():
 '''
 Dibuja las casillas del tablero
 '''
+
+
 def dibujarTablero(pantalla):
     color1 = p.Color("#C5742A")
     color2 = p.Color("#EBCCAA")
@@ -85,29 +105,36 @@ def dibujarTablero(pantalla):
                 color = color2
             else:
                 color = color1
-            p.draw.rect(pantalla, color, p.Rect(c * SQ_SIZE, f * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            p.draw.rect(pantalla, color, p.Rect(
+                c * SQ_SIZE, f * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 '''
 Dibuja las piezas en el tablero utilizando el estado actual => Estado de juego
 '''
+
+
 def dibujarPiezas(pantalla, tablero):
     for f in range(DIMENSION):
         for c in range(DIMENSION):
             pieza = tablero[f][c]
             if pieza != "--":
-                pantalla.blit(IMAGENES[pieza], p.Rect(c * SQ_SIZE, f * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                pantalla.blit(IMAGENES[pieza], p.Rect(
+                    c * SQ_SIZE, f * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 '''
 Responsable de todos los graficos que estan dentro del estado de juego actual
 '''
+
+
 def dibujarEstado(pantalla, estadoJuego):
     # Dibuja el tablero
     dibujarTablero(pantalla)
     # Previsualización posibles direcciones(Luego)
 
     dibujarPiezas(pantalla, estadoJuego.tablero)
+
     # Dibujando piezas en los casilleros de los extremos
 
 
